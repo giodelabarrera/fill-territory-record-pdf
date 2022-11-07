292e4d9b31a5dea77953955a4588f4ac5450d510
Giorgio de la Barrera <giodelabarrera@gmail.com>
2022-11-07 22:38:32 +0100

    Create get field name functions

--- index.js
+++ index.js
@@ -102,6 +102,39 @@ Date_381
 // is number 1
 // Name_001
 
-export function getRegistryCellMeta(columnNumber, rowNumber) {}
+export function getPublisherFieldName(registryCell) {
+  const NUMBER_OF_COLUMNS = 5;
+  const { column, row } = registryCell;
+
+  const cellNumber = column + NUMBER_OF_COLUMNS * (row - 1);
+  const cellNumberStr = String(cellNumber).padStart(3, "0");
+  return `Name_${cellNumberStr}`;
+}
+
+export function getStartDateFieldName(registryCell) {
+  const NUMBER_OF_COLUMNS = 10;
+  const { column, row } = registryCell;
+
+  const cellNumber = column + (column - 1) + NUMBER_OF_COLUMNS * (row - 1);
+  const cellNumberStr = String(cellNumber).padStart(3, "0");
+  return `Date_${cellNumberStr}`;
+}
+
+export function getEndDateFieldName(registryCell) {
+  const NUMBER_OF_COLUMNS = 10;
+  const { column, row } = registryCell;
+
+  const cellNumber = column * 2 + NUMBER_OF_COLUMNS * (row - 1);
+  const cellNumberStr = String(cellNumber).padStart(3, "0");
+  return `Date_${cellNumberStr}`;
+}
+
+export function getRegistryFieldNames(registryCell) {
+  return {
+    publisher: getPublisherFieldName(registryCell),
+    startDate: getStartDateFieldName(registryCell),
+    endDate: getEndDateFieldName(registryCell),
+  };
+}
 
 export function addRegistry(document, columnNumber, columnFile, registry) {}
--- index.test.js
+++ index.test.js
@@ -1,27 +1,58 @@
-import { getRegistryCellMeta } from "./index";
+import {
+  getEndDateFieldName,
+  getPublisherFieldName,
+  getRegistryFieldNames,
+  getStartDateFieldName,
+} from "./index";
+
+test("retrieve publisher field name", () => {
+  expect(getPublisherFieldName({ column: 1, row: 1 })).toEqual("Name_001");
+  expect(getPublisherFieldName({ column: 1, row: 2 })).toEqual("Name_006");
+  expect(getPublisherFieldName({ column: 2, row: 1 })).toEqual("Name_002");
+  expect(getPublisherFieldName({ column: 5, row: 1 })).toEqual("Name_005");
+  expect(getPublisherFieldName({ column: 5, row: 8 })).toEqual("Name_040");
+});
+
+test("retrieve start date field name", () => {
+  expect(getStartDateFieldName({ column: 1, row: 1 })).toEqual("Date_001");
+  expect(getStartDateFieldName({ column: 1, row: 2 })).toEqual("Date_011");
+  expect(getStartDateFieldName({ column: 2, row: 1 })).toEqual("Date_003");
+  expect(getStartDateFieldName({ column: 5, row: 1 })).toEqual("Date_009");
+  expect(getStartDateFieldName({ column: 5, row: 8 })).toEqual("Date_079");
+});
+
+test("retrieve end date field name", () => {
+  expect(getEndDateFieldName({ column: 1, row: 1 })).toEqual("Date_002");
+  expect(getEndDateFieldName({ column: 1, row: 2 })).toEqual("Date_012");
+  expect(getEndDateFieldName({ column: 2, row: 1 })).toEqual("Date_004");
+  expect(getEndDateFieldName({ column: 5, row: 1 })).toEqual("Date_010");
+  expect(getEndDateFieldName({ column: 5, row: 8 })).toEqual("Date_080");
+});
 
 test("retrieve registry cell meta", () => {
-  expect(getRegistryCellMeta(1, 1)).toEqual({
-    publisher: "Name_001",
-    startDate: "Date_001",
-    endDate: "Date_002",
+  expect(getRegistryFieldNames({ column: 1, row: 12 })).toEqual({
+    publisher: "Name_056",
+    startDate: "Date_111",
+    endDate: "Date_112",
   });
-
-  expect(getRegistryCellMeta(1, 2)).toEqual({
-    publisher: "Name_006",
-    startDate: "Date_011",
-    endDate: "Date_012",
+  expect(getRegistryFieldNames({ column: 2, row: 12 })).toEqual({
+    publisher: "Name_057",
+    startDate: "Date_113",
+    endDate: "Date_114",
   });
-
-  expect(getRegistryCellMeta(2, 1)).toEqual({
-    publisher: "Name_002",
-    startDate: "Date_003",
-    endDate: "Date_004",
+  expect(getRegistryFieldNames({ column: 3, row: 12 })).toEqual({
+    publisher: "Name_058",
+    startDate: "Date_115",
+    endDate: "Date_116",
   });
-
-  expect(getRegistryCellMeta(5, 11)).toEqual({
-    publisher: "Name_055",
-    startDate: "Date_109",
-    endDate: "Date_110",
+  expect(getRegistryFieldNames({ column: 4, row: 12 })).toEqual({
+    publisher: "Name_059",
+    startDate: "Date_117",
+    endDate: "Date_118",
+  });
+  expect(getRegistryFieldNames({ column: 5, row: 12 })).toEqual({
+    publisher: "Name_060",
+    startDate: "Date_119",
+    endDate: "Date_120",
   });
 });
