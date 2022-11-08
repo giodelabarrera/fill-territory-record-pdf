da18efc34ab4aed5a763747ce97458f2fe009a59
Giorgio de la Barrera <giodelabarrera@gmail.com>
2022-11-08 23:52:26 +0100

    Add main by functions

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
 const { PDFDocument } = require("pdf-lib");
-const { writeFileSync, readFileSync } = require("fs");
+const { writeFileSync, readFileSync, createReadStream } = require("fs");
+const path = require("path");
 
 const { fillPDF } = require("./pdfUtils");
-const { createNumbersFromTo } = require("./utils");
+const { mapCSVRowsToMap } = require("./csvUtils");
 
-const registryMap = new Map();
-registryMap.set(11, [
-  {
-    publisher: "Giorgio de la Barrera",
-    startDate: new Date("2022-09-01"),
-    endDate: new Date("2022-11-02"),
-  },
-  {
-    publisher: "Dalila Taborda",
-    startDate: new Date("2022-09-03"),
-    endDate: undefined,
-  },
-]);
-registryMap.set(12, [
-  {
-    publisher: "Jacob de la Barrera",
-    startDate: new Date("2022-09-04"),
-    endDate: new Date("2022-11-05"),
-  },
-]);
-createNumbersFromTo(13, 20).forEach((number) => {
-  registryMap.set(number, []);
-});
+const FROM_TERRITORY = 1;
+const TO_TERRITORY = 10;
 
 (async () => {
+  const csvPath = path.resolve(__dirname, "registries.csv");
+  const csvStream = createReadStream(csvPath);
+  const registryMap = await mapCSVRowsToMap(
+    csvStream,
+    FROM_TERRITORY,
+    TO_TERRITORY
+  );
+
   const document = await PDFDocument.load(readFileSync("./S-13_S.pdf"));
   fillPDF(document, registryMap);
-
   writeFileSync("S-13_S_fill.pdf", await document.save());
-})().catch((err) => console.log(err));
+})();
