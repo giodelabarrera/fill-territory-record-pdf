710b6ef8bb14d9b2716b2856cde1fb1b66d5fe3e
Giorgio de la Barrera <giodelabarrera@gmail.com>
2022-11-08 08:43:22 +0100

    Create csv utils

---
+++ csvUtils.js
@@ -0,0 +1,44 @@
+const csv = require("@fast-csv/parse");
+
+const { createNumbersFromTo } = require("./utils");
+
+async function mapCSVRowsToMap(csvStream, fromTerritory, toTerritory) {
+  return new Promise((res, rej) => {
+    const registryMap = new Map();
+    createNumbersFromTo(fromTerritory, toTerritory).forEach(
+      (territoryNumber) => {
+        registryMap.set(territoryNumber, []);
+      }
+    );
+
+    csv
+      .parseStream(csvStream, { headers: true })
+      .on("error", rej)
+      .on("data", (row) => {
+        const {
+          territory: originalTerritory,
+          publisher,
+          started_at,
+          finished_at,
+        } = row;
+        const territory = Number(originalTerritory);
+        if (registryMap.has(territory)) {
+          registryMap.set(territory, [
+            ...registryMap.get(territory),
+            {
+              publisher,
+              startDate: new Date(started_at),
+              endDate: finished_at ? new Date(finished_at) : undefined,
+            },
+          ]);
+        }
+      })
+      .on("end", () => {
+        res(registryMap);
+      });
+  });
+}
+
+module.exports = {
+  mapCSVRowsToMap,
+};
