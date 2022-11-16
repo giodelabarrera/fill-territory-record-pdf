e680f80f24c1a51120243ee0b7a4c597cb745c97
Giorgio de la Barrera <giodelabarrera@gmail.com>
2022-11-16 16:33:00 +0100

    resolve dirname path

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 import { PDFDocument } from "pdf-lib";
-import { writeFileSync, readFileSync, createReadStream } from "fs";
-import path from "path";
-import * as url from "url";
+import { createReadStream } from "fs";
+import { writeFile, readFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 import { fillPDF } from "./pdfUtils.js";
 import { mapCSVRowsToMap } from "./csvUtils.js";
 
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 
 const FROM_TERRITORY = 1;
 const TO_TERRITORY = 10;
 
-(async () => {
-  const csvPath = path.resolve(__dirname, "registries.csv");
-  const csvStream = createReadStream(csvPath);
-  const registryMap = await mapCSVRowsToMap(
-    csvStream,
-    FROM_TERRITORY,
-    TO_TERRITORY
-  );
+const csvPath = join(__dirname, "registries.csv");
+const csvStream = createReadStream(csvPath);
+const registryMap = await mapCSVRowsToMap(
+  csvStream,
+  FROM_TERRITORY,
+  TO_TERRITORY
+);
 
-  const document = await PDFDocument.load(readFileSync("./S-13_S.pdf"));
-  fillPDF(document, registryMap);
-  writeFileSync(
-    `S-13_S_${FROM_TERRITORY}_${TO_TERRITORY}.pdf`,
-    await document.save()
-  );
-})();
+const pdfBuffer = await readFile("./S-13_S.pdf");
+const document = await PDFDocument.load(pdfBuffer);
+fillPDF(document, registryMap);
+await writeFile(
+  join(__dirname, `S-13_S_${FROM_TERRITORY}_${TO_TERRITORY}.pdf`),
+  await document.save()
+);
