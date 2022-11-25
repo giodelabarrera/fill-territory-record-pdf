af767f98ca4d62577ce8c9492935b87b4530824f
Giorgio de la Barrera <giodelabarrera@gmail.com>
2022-11-25 15:37:13 +0100

    Create main filled pdf

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
-const { PDFDocument } = require("pdf-lib");
-const { createReadStream } = require("fs");
-const { writeFile, readFile } = require("fs/promises");
+#!/usr/bin/env node
 const { join } = require("path");
 
-const { fillPDF } = require("./pdf-utils.js");
-const { mapCSVRowsToMap } = require("./csv-utils.js");
+const { createFilledPDF } = require("./create-filled-pdf.js");
 
 const FROM_TERRITORY = 1;
-const TO_TERRITORY = 10;
+const csvPath = join(__dirname, "assets", "registries.csv");
 
-(async () => {
-  const csvPath = join(__dirname, "assets", "registries.csv");
-  const csvStream = createReadStream(csvPath);
-  const registryMap = await mapCSVRowsToMap(
-    csvStream,
-    FROM_TERRITORY,
-    TO_TERRITORY
-  );
-
-  const pdfBuffer = await readFile(join(__dirname, "assets", "S-13_S.pdf"));
-  const document = await PDFDocument.load(pdfBuffer);
-  fillPDF(document, registryMap);
-  await writeFile(
-    join(__dirname, `S-13_S_${FROM_TERRITORY}_${TO_TERRITORY}.pdf`),
-    await document.save()
-  );
-})();
+createFilledPDF(csvPath, FROM_TERRITORY);
